0fb4c88daa2ac747818e1a7b87558d341fad31b2
Hideaki Kimura <hideaki.kimura@hp.com>
2014-05-16 14:18:54 -0700

    in case make dox wasnot run yet

--- general_projects.cmake
+++ general_projects.cmake
@@ -3,19 +3,10 @@
 # Those projects are NOT intended to be included from user programs.
 #############################################################################
 if (NOT "${CMAKE_CXX_FLAGS}" MATCHES "\\-std\\=c\\+\\+11")
-    message(FATAL_ERROR "These projects assume have to be compiled from super build.")
+    message(FATAL_ERROR "These projects have to be compiled from super build.")
 endif ()
-
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_SOURCE_DIR}/foedus-core/cmake)
-
-
-find_package(Numa REQUIRED)
-find_package(Threads REQUIRED)
 
 include_directories(
     ${CMAKE_SOURCE_DIR}/foedus-core/include
     ${CMAKE_SOURCE_DIR}/third_party/gflags-2.1.1/include
     ${CMAKE_BINARY_DIR}/foedus-core/third_party/glog-0.3.3/src)
-
-# use this in target_link_libraries for the programs
-set(GENERAL_LIB foedus-core ${NUMA_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
