b79c3616136a9663b5c86d38a5312baf4af9ef88
Hideaki Kimura <hideaki.kimura@hp.com>
2014-08-22 13:46:45 -0700

    Prefer libnuma from local

--- foedus-core/cmake/FindNuma.cmake
+++ foedus-core/cmake/FindNuma.cmake
@@ -4,10 +4,10 @@
 #  NUMA_LIBRARY     : Library path of numa library
 #  NUMA_FOUND       : True if found.
 FIND_PATH(NUMA_INCLUDE_DIR NAME numa.h
-  PATHS /opt/local/include /usr/local/include /usr/include)
+  HINTS $ENV{HOME}/local/include /opt/local/include /usr/local/include /usr/include)
 
 FIND_LIBRARY(NUMA_LIBRARY NAME numa
-  PATHS /usr/lib64 /usr/local/lib64 /opt/local/lib64 /usr/lib /usr/local/lib /opt/local/lib
+  HINTS $ENV{HOME}/local/lib64 $ENV{HOME}/local/lib /usr/local/lib64 /usr/local/lib /opt/local/lib64 /opt/local/lib /usr/lib64 /usr/lib
 )
 
 IF (NUMA_INCLUDE_DIR AND NUMA_LIBRARY)
