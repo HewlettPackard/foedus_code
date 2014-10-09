6a0c36aa2a81928a219ed86d1373415337a21c0e
Hideaki Kimura <hideaki.kimura@hp.com>
2014-10-08 17:05:26 -0700

    debug-out function for DualPagePointer

--- foedus-core/src/foedus/storage/storage_id.cpp
+++ foedus-core/src/foedus/storage/storage_id.cpp
@@ -6,11 +6,35 @@
 
 #include <ostream>
 
+#include "foedus/assorted/assorted_func.hpp"
+
 namespace foedus {
 namespace storage {
-std::ostream& operator<<(std::ostream& o, const DualPagePointer& /*v*/) {
-  // TODO(Hideaki) implement
-  o << "DualPagePointer:" << std::endl;
+
+void describe_snapshot_pointer(std::ostream* o_ptr, SnapshotPagePointer pointer) {
+  std::ostream& o = *o_ptr;
+  o << "<SnapshotPagePointer>";
+  o << "<SnapshotId>" << extract_snapshot_id_from_snapshot_pointer(pointer) << "</SnapshotId>";
+  o << "<Node>" << static_cast<int>(extract_numa_node_from_snapshot_pointer(pointer)) << "</Node>";
+  o << "<PageId>" << extract_local_page_id_from_snapshot_pointer(pointer) << "</PageId>";
+  o << "</SnapshotPagePointer>";
+}
+
+void describe_volatile_pointer(std::ostream* o_ptr, VolatilePagePointer pointer) {
+  std::ostream& o = *o_ptr;
+  o << "<VolatilePagePointer>";
+  o << "<Node>" << static_cast<int>(pointer.components.numa_node) << "</Node>";
+  o << "<Offset>" << pointer.components.offset << "</Offset>";
+  o << "<Flags>" << assorted::Hex(pointer.components.flags) << "</Flags>";
+  o << "<ModCount>" << pointer.components.mod_count << "</ModCount>";
+  o << "</VolatilePagePointer>";
+}
+
+std::ostream& operator<<(std::ostream& o, const DualPagePointer& v) {
+  o << "<DualPagePointer>";
+  describe_snapshot_pointer(&o, v.snapshot_pointer_);
+  describe_volatile_pointer(&o, v.volatile_pointer_);
+  o << "</DualPagePointer>";
   return o;
 }
 }  // namespace storage
