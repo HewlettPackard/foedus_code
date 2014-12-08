a86198a1a4420104b4b65dff52625105e218b2c0
Hideaki Kimura <hideaki.kimura@hp.com>
2014-12-08 10:26:50 -0800

    minor

--- foedus-core/include/foedus/log/namespace-info.hpp
+++ foedus-core/include/foedus/log/namespace-info.hpp
@@ -11,26 +11,26 @@
  * @details
  * This package contains classes that control transactional logging.
  *
- * @section DECENTRAL_LOG Decentralized Logging
+ * @par Decentralized Logging
  * Unlike traditional log manager in DBMS, this log manager is \e decentralized, meaning
  * each log writer writes to its own file concurrently. This eliminates the bottleneck in
  * log manager when there are a large number of cores.
  * The basic idea to guarantee serializability is the epoch-based commit protocol, which does the
  * check on all loggers before returning the results to client in a way similar to group-commit.
  *
- * @section THREAD_BUFFER Thread-private log buffer
+ * @par Thread-private log buffer
  * Each ThreadLoadBugger instance maintains a thread-local log buffer that is filled by
  * the thread without any synchronization or blocking. The logger collects them and writes
  * them out to log files. A single log writer handles one or more transactional threads (cores),
  * and a single NUMA node hosts one or more log writers.
  * @see foedus::log::ThreadLogBuffer
  *
- * @section LOGGER Log Writer
+ * @par Log Writer
  * Each Logger instance writes out files suffixed with ordinal (eg ".0", ".1"...).
  * The older logs files are deactivated and deleted after log gleaner consumes them.
  * @see foedus::log::Logger
  *
- * @section DURABLEEP Global Durable Epoch
+ * @par Global Durable Epoch
  * An important job of Log Manager is to maintain the \e global durable epoch.
  * Log manager is the module to determine when it's safe to advance the global durable epoch.
  * It makes sure all loggers flushed their logs up to the epoch, invoked required fsync(),
