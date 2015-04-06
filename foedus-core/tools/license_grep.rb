0c77438dbeec9e43962b19f01b9fb175beb4513c
Hideaki Kimura <hideaki.kimura@hp.com>
2015-04-06 14:52:03 -0700

    Switched to GPL with classpath exception

--- foedus-core/tools/license_grep.rb
+++ foedus-core/tools/license_grep.rb
@@ -1,4 +1,12 @@
-/*
+#!/usr/bin/env ruby
+
+require 'colorize'
+
+@fromstr = "/*
+ * Copyright (c) 2014, Hewlett-Packard Development Company, LP.
+ * The license and distribution terms for this file are placed in LICENSE.txt.
+ */"
+@tostr = "/*
  * Copyright (c) 2014-2015, Hewlett-Packard Development Company, LP.
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -12,27 +20,20 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * HP designates this particular file as subject to the "Classpath" exception
+ * HP designates this particular file as subject to the \"Classpath\" exception
  * as provided by HP in the LICENSE.txt file that accompanied this code.
- */
-/**
- * @file foedus/tpcc/tpcc.cpp
- * @brief TPC-C experiment
- * @author kimurhid
- * @date 2014/08/01
- * @details
- * This implements the popular TPC-C benchmark.
- *
- * @section ENVIRONMENTS Environments
- *
- * @section OTHER Other notes
- *
- * @section RESULTS Latest Results
- */
-#include "foedus/tpcc/tpcc.hpp"
+ */"
+puts "Replacing \n#{@fromstr}\n with \n#{@tostr}\n..."
 
-#include "foedus/tpcc/tpcc_driver.hpp"
-
-int main(int argc, char **argv) {
-  return foedus::tpcc::driver_main(argc, argv);
-}
+@cpps = Dir.glob("**/*.*pp").each do|path|
+    org = File.read(path)
+    if org.index(@tostr) == 0
+        puts ("Already OK: " + path).blue
+    elsif org.index(@fromstr) == 0
+        modified = @tostr + org[@fromstr.length..-1]
+        File.write(path, modified)
+        puts ("Replaced: " + path).yellow
+    else
+        puts ("WTF :" + path).red
+    end
+end
