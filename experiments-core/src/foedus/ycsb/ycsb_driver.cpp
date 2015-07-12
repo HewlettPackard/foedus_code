66566638fdce7f0fcf88475915592206937a1cc6
Tianzheng Wang <tzwang@cs.toronto.edu>
2015-07-12 14:58:36 -0700

    Trivial format change for YCSB workloads.

--- experiments-core/src/foedus/ycsb/ycsb_driver.cpp
+++ experiments-core/src/foedus/ycsb/ycsb_driver.cpp
@@ -69,21 +69,12 @@ DEFINE_int64(duration_micro, 1000000, "Duration of benchmark in microseconds.");
 DEFINE_string(workload, "A", "YCSB workload; choose A/B/C/D/E.");
 DEFINE_string(non_scan_table_type, "hash", "Storage type (hash or masstree) of user_table for non-scan transactions.");
 
-// Workload A - 50% read, 50% update
-YcsbWorkload YcsbWorkloadA('A', 50U, 100U, 0, 0);
 
-// Workload B - 95% read, 5% update
-YcsbWorkload YcsbWorkloadB('B', 95U, 100U, 0, 0);
-
-// Workload C - 100% read
-YcsbWorkload YcsbWorkloadC('C', 100U, 0, 0, 0);
-
-// Workload D - 95% read, 5% insert
-YcsbWorkload YcsbWorkloadD('D', 95U, 0, 100U, 0);
-
-// Workload E - 5% insert, 95% scan
-YcsbWorkload YcsbWorkloadE('E', 0, 0, 5U, 100U);
-
+YcsbWorkload YcsbWorkloadA('A', 50U,  100U, 0,    0);     // Workload A - 50% read, 50% update
+YcsbWorkload YcsbWorkloadB('B', 95U,  100U, 0,    0);     // Workload B - 95% read, 5% update
+YcsbWorkload YcsbWorkloadC('C', 100U, 0,    0,    0);     // Workload C - 100% read
+YcsbWorkload YcsbWorkloadD('D', 95U,  0,    100U, 0);     // Workload D - 95% read, 5% insert
+YcsbWorkload YcsbWorkloadE('E', 0,    0,    5U,   100U);  // Workload E - 5% insert, 95% scan
 
 YcsbRecord::YcsbRecord(char value) {
   // So just write some arbitrary characters provided, no need to use rnd
