6ae625e374dd20d1f4094cd332210baac871163e
Hideaki Kimura <hideaki.kimura@hp.com>
2015-10-04 12:36:50 -0700

    Unit test to reproduce #100.
    The trick was to generate log records that exactly fill the mapper buffer.
    2048-bytes log records did the job.
    Out of the 4 testcases, TwoIterationsUnlucky fails.

--- tests-core/src/foedus/snapshot/test_mapper_io.cpp
+++ tests-core/src/foedus/snapshot/test_mapper_io.cpp
@@ -47,21 +47,35 @@ DEFINE_TEST_CASE_PACKAGE(MapperIoTest, foedus.snapshot);
 // Let's use a quite fat payload.
 // kTwoIterationLogs * kPayload = 3 MB (+some), surely more than 2MB.
 // kOneIterationLogs * kPayload = 0.6 MB (+some), surely less than 2MB.
-const uint32_t kPayload = 3000;
+const uint32_t kPayloadUsual = 3000;
+const uint32_t kPayloadUnlucky = 2016;
+// Also, we test two payload size so that "unlucky" version makes the log record
+// exactly 2048 bytes. The aforementioned bug happened when mapper-io buffer
+// (un)luckily ends with a complete log record (not spanning to next buffer-read).
+
 const uint32_t kTwoIterationsLogs = 1024;
 const uint32_t kOneIterationLogs = 200;
 const uint32_t kRecords = 16;  // To speed up the test, actually we only use the first record
 const storage::StorageName kName("test");
 
-ErrorStack load_impl(const proc::ProcArguments& args, uint32_t log_count) {
+struct TestInput {
+  uint32_t log_count_;
+  uint32_t payload_size_;
+};
+
+ErrorStack load_task(const proc::ProcArguments& args) {
+  const TestInput* input = reinterpret_cast<const TestInput*>(args.input_buffer_);
+  const uint32_t log_count = input->log_count_;
+  const uint32_t payload_size = input->payload_size_;
   thread::Thread* context = args.context_;
   storage::array::ArrayStorage array(args.engine_, kName);
   ASSERT_ND(array.exists());
   xct::XctManager* xct_manager = args.engine_->get_xct_manager();
 
   Epoch commit_epoch;
-  char payload[kPayload];
-  std::memset(payload, 0, kPayload);
+  ASSERT_ND(payload_size <= (1U << 12));
+  char payload[1U << 12];
+  std::memset(payload, 0, payload_size);
   for (uint32_t i = 0; i < log_count;) {
     const uint32_t kLogsPerBatch = 50;
     WRAP_ERROR_CODE(xct_manager->begin_xct(context, xct::kSerializable));
@@ -84,46 +98,39 @@ ErrorStack load_impl(const proc::ProcArguments& args, uint32_t log_count) {
   WRAP_ERROR_CODE(xct_manager->wait_for_commit(commit_epoch));
   return kRetOk;
 }
-ErrorStack load_two_iterations_task(const proc::ProcArguments& args) {
-  CHECK_ERROR(load_impl(args, kTwoIterationsLogs));
-  return kRetOk;
-}
-ErrorStack load_one_iteration_task(const proc::ProcArguments& args) {
-  CHECK_ERROR(load_impl(args, kOneIterationLogs));
-  return kRetOk;
-}
 
-ErrorStack verify_impl(const proc::ProcArguments& args, uint32_t log_count) {
+ErrorStack verify_task(const proc::ProcArguments& args) {
+  const TestInput* input = reinterpret_cast<const TestInput*>(args.input_buffer_);
+  const uint32_t log_count = input->log_count_;
+  const uint32_t payload_size = input->payload_size_;
+  ASSERT_ND(payload_size <= (1U << 12));
+
   thread::Thread* context = args.context_;
   storage::array::ArrayStorage array(args.engine_, kName);
   ASSERT_ND(array.exists());
   xct::XctManager* xct_manager = args.engine_->get_xct_manager();
 
-  char payload_correct[kPayload];
-  std::memset(payload_correct, 0, kPayload);
+  char payload_correct[1U << 12];
+  std::memset(payload_correct, 0, payload_size);
   *reinterpret_cast<uint64_t*>(payload_correct) = static_cast<uint64_t>(log_count - 1);
 
-  char payload[kPayload];
+  char payload[1U << 12];
   WRAP_ERROR_CODE(xct_manager->begin_xct(context, xct::kSerializable));
   WRAP_ERROR_CODE(array.get_record(context, 0, payload));
   uint64_t payload_head = *reinterpret_cast<const uint64_t*>(payload);
   EXPECT_EQ(log_count, payload_head + 1U);
-  EXPECT_EQ(std::string(payload_correct, kPayload), std::string(payload, kPayload));
+  EXPECT_EQ(std::string(payload_correct, payload_size), std::string(payload, payload_size));
 
   Epoch commit_epoch;
   WRAP_ERROR_CODE(xct_manager->precommit_xct(context, &commit_epoch));
   return kRetOk;
 }
-ErrorStack verify_two_iterations_task(const proc::ProcArguments& args) {
-  CHECK_ERROR(verify_impl(args, kTwoIterationsLogs));
-  return kRetOk;
-}
-ErrorStack verify_one_iteration_task(const proc::ProcArguments& args) {
-  CHECK_ERROR(verify_impl(args, kOneIterationLogs));
-  return kRetOk;
-}
 
-void test_run(bool two_iterations) {
+void test_run(bool two_iterations, bool unlucky_size) {
+  TestInput input;
+  input.log_count_ = two_iterations ? kTwoIterationsLogs : kOneIterationLogs;
+  input.payload_size_ = unlucky_size ? kPayloadUnlucky : kPayloadUsual;
+
   EngineOptions options = get_tiny_options();
   options.thread_.thread_count_per_group_ = 1;
   options.log_.loggers_per_node_ = 1;
@@ -131,19 +138,14 @@ void test_run(bool two_iterations) {
 
   {
     Engine engine(options);
-    if (two_iterations) {
-      engine.get_proc_manager()->pre_register("load_task", load_two_iterations_task);
-      engine.get_proc_manager()->pre_register("verify_task", verify_two_iterations_task);
-    } else {
-      engine.get_proc_manager()->pre_register("load_task", load_one_iteration_task);
-      engine.get_proc_manager()->pre_register("verify_task", verify_one_iteration_task);
-    }
+    engine.get_proc_manager()->pre_register("load_task", load_task);
+    engine.get_proc_manager()->pre_register("verify_task", verify_task);
     COERCE_ERROR(engine.initialize());
     {
       UninitializeGuard guard(&engine);
       storage::array::ArrayStorage out;
       Epoch commit_epoch;
-      storage::array::ArrayMetadata meta(kName, kPayload, kRecords);
+      storage::array::ArrayMetadata meta(kName, input.payload_size_, kRecords);
 
       // allow dropping all volatile pages to test purely snapshot-page only cases.
       meta.snapshot_drop_volatile_pages_threshold_ = 0;
@@ -152,32 +154,31 @@ void test_run(bool two_iterations) {
       EXPECT_TRUE(commit_epoch.is_valid());
 
       thread::ThreadPool* pool = engine.get_thread_pool();
-      COERCE_ERROR(pool->impersonate_synchronous("load_task"));
-      COERCE_ERROR(pool->impersonate_synchronous("verify_task"));
+      COERCE_ERROR(pool->impersonate_synchronous("load_task", &input, sizeof(input)));
+      COERCE_ERROR(pool->impersonate_synchronous("verify_task", &input, sizeof(input)));
       engine.get_snapshot_manager()->trigger_snapshot_immediate(true);
-      COERCE_ERROR(pool->impersonate_synchronous("verify_task"));
+      COERCE_ERROR(pool->impersonate_synchronous("verify_task", &input, sizeof(input)));
       COERCE_ERROR(engine.uninitialize());
     }
   }
   {
     Engine engine(options);
-    if (two_iterations) {
-      engine.get_proc_manager()->pre_register("verify_task", verify_two_iterations_task);
-    } else {
-      engine.get_proc_manager()->pre_register("verify_task", verify_one_iteration_task);
-    }
+    engine.get_proc_manager()->pre_register("verify_task", verify_task);
     COERCE_ERROR(engine.initialize());
     {
       UninitializeGuard guard(&engine);
-      COERCE_ERROR(engine.get_thread_pool()->impersonate_synchronous("verify_task"));
+      thread::ThreadPool* pool = engine.get_thread_pool();
+      COERCE_ERROR(pool->impersonate_synchronous("verify_task", &input, sizeof(input)));
       COERCE_ERROR(engine.uninitialize());
     }
   }
   cleanup_test(options);
 }
 
-TEST(MapperIoTest, OneIteration) { test_run(false); }
-TEST(MapperIoTest, TwoIterations) { test_run(true); }
+TEST(MapperIoTest, OneIteration) { test_run(false, false); }
+TEST(MapperIoTest, TwoIterations) { test_run(true, false); }
+TEST(MapperIoTest, OneIterationUnlucky) { test_run(false, true); }
+TEST(MapperIoTest, TwoIterationsUnlucky) { test_run(true, true); }
 
 }  // namespace snapshot
 }  // namespace foedus
