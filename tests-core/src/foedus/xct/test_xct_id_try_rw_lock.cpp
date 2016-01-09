52cd76f1960adaae60f2e029ef0eb0b81b7a57c0
Tianzheng Wang <tzwang@cs.toronto.edu>
2016-01-09 14:48:21 -0500

    fix rw-lock test case.

--- tests-core/src/foedus/xct/test_xct_id_try_rw_lock.cpp
+++ tests-core/src/foedus/xct/test_xct_id_try_rw_lock.cpp
@@ -117,51 +117,6 @@ ErrorStack no_conflict_task(const proc::ProcArguments& args) {
   return foedus::kRetOk;
 }
 
-ErrorStack conflict_task(const proc::ProcArguments& args) {
-  thread::Thread* context = args.context_;
-  EXPECT_EQ(args.input_len_, sizeof(int));
-  int id = *reinterpret_cast<const int*>(args.input_buffer_);
-  int l = id < kThreads / 2 ? id : id - kThreads / 2;
-  XctManager* xct_manager = context->get_engine()->get_xct_manager();
-  WRAP_ERROR_CODE(xct_manager->begin_xct(context, kSerializable));
-  McsBlockIndex block = 0;
-  if (id % 2 == 0) {
-  retry1:
-    while (!context->mcs_try_acquire_reader_lock(keys[id].get_key_lock(), &block)) {}
-    if (!context->mcs_retry_acquire_reader_lock(keys[id].get_key_lock(), block, true)) {
-      goto retry1;
-    }
-  } else {
-  retry2:
-    while (!context->mcs_try_acquire_writer_lock(keys[id].get_key_lock(), &block)) {}
-    if (!context->mcs_retry_acquire_writer_lock(keys[id].get_key_lock(), block, true)) {
-      goto retry2;
-    }
-  }
-  locked[id] = true;
-  ++locked_count;
-  while (!signaled) {
-    sleep_enough();
-    assorted::memory_fence_acquire();
-  }
-  if (block) {
-    if (id % 2 == 0) {
-      acquired_reads++;
-      context->mcs_release_reader_lock(keys[l].get_key_lock(), block);
-    } else {
-      acquired_writes++;
-      context->mcs_release_writer_lock(keys[l].get_key_lock(), block);
-    }
-  }
-  WRAP_ERROR_CODE(xct_manager->abort_xct(context));
-  done[id] = true;
-  ++done_count;
-  std::cout << "Acquired writes: " << acquired_writes << ", "
-    << "Acquired reads: " << acquired_reads << std::endl;
-  return foedus::kRetOk;
-}
-
-
 ErrorStack random_task(const proc::ProcArguments& args) {
   thread::Thread* context = args.context_;
   EXPECT_EQ(args.input_len_, sizeof(int));
@@ -173,14 +128,14 @@ ErrorStack random_task(const proc::ProcArguments& args) {
     uint32_t k = r.uniform_within(0, kKeys - 1);
     McsBlockIndex block = 0;
     if (id % 2 == 0) {
-      while (!context->mcs_try_acquire_reader_lock(keys[id].get_key_lock(), &block)) {}
-      if (context->mcs_retry_acquire_reader_lock(keys[id].get_key_lock(), block, true)) {
+      while (!context->mcs_try_acquire_reader_lock(keys[k].get_key_lock(), &block)) {}
+      if (context->mcs_retry_acquire_reader_lock(keys[k].get_key_lock(), block, true)) {
         acquired_reads++;
         context->mcs_release_reader_lock(keys[k].get_key_lock(), block);
       }
     } else {
-      while (!context->mcs_try_acquire_writer_lock(keys[id].get_key_lock(), &block)) {}
-      if (context->mcs_retry_acquire_writer_lock(keys[id].get_key_lock(), block, true)) {
+      while (!context->mcs_try_acquire_writer_lock(keys[k].get_key_lock(), &block)) {}
+      if (context->mcs_retry_acquire_writer_lock(keys[k].get_key_lock(), block, true)) {
         acquired_writes++;
         context->mcs_release_writer_lock(keys[k].get_key_lock(), block);
       }
@@ -245,80 +200,6 @@ TEST(XctIdRwTryLockTest, NoConflict) {
       COERCE_ERROR(sessions[i].get_result());
       sessions[i].release();
     }
-    COERCE_ERROR(engine.uninitialize());
-  }
-  cleanup_test(options);
-}
-
-TEST(XctIdRwTryLockTest, Conflict) {
-  EngineOptions options = get_tiny_options();
-  options.thread_.thread_count_per_group_ = kThreads;
-  Engine engine(options);
-  engine.get_proc_manager()->pre_register("conflict_task", conflict_task);
-  COERCE_ERROR(engine.initialize());
-  {
-    UninitializeGuard guard(&engine);
-    init();
-    std::vector<thread::ImpersonateSession> sessions;
-    for (int i = 0; i < kThreads / 2; ++i) {
-      thread::ImpersonateSession session;
-      EXPECT_TRUE(engine.get_thread_pool()->impersonate(
-        "conflict_task",
-        &i,
-        sizeof(i),
-        &session));
-      EXPECT_TRUE(session.is_valid());
-      ASSERT_ND(session.is_valid());
-      sessions.emplace_back(std::move(session));
-    }
-    while (locked_count < kThreads / 2) {
-      sleep_enough();
-    }
-    for (int i = kThreads / 2; i < kThreads; ++i) {
-      thread::ImpersonateSession session;
-      EXPECT_TRUE(engine.get_thread_pool()->impersonate(
-        "conflict_task",
-        &i,
-        sizeof(i),
-        &session));
-      EXPECT_TRUE(session.is_valid());
-      ASSERT_ND(session.is_valid());
-      sessions.emplace_back(std::move(session));
-    }
-    for (int i = 0; i < 4; ++i) {
-      sleep_enough();
-    }
-    assorted::memory_fence_acquire();
-    /*
-    for (int i = 0; i < kThreads; ++i) {
-      int l = i < kThreads / 2 ? i : i - kThreads / 2;
-      EXPECT_FALSE(keys[l].xct_id_.is_valid()) << i;
-      EXPECT_FALSE(keys[l].is_deleted()) << i;
-      EXPECT_TRUE(keys[l].is_keylocked()) << i;
-      EXPECT_FALSE(keys[l].is_moved()) << i;
-      if (i < kThreads / 2) {
-        EXPECT_TRUE(locked[i]) << i;
-      } else {
-        EXPECT_FALSE(locked[i]) << i;
-      }
-      EXPECT_FALSE(done[i]) << i;
-    }
-    */
-    assorted::memory_fence_release();
-    signaled = true;
-    while (done_count < kThreads) {
-      sleep_enough();
-    }
-    for (int i = 0; i < kThreads; ++i) {
-      EXPECT_TRUE(locked[i]) << i;
-      EXPECT_TRUE(done[i]) << i;
-      EXPECT_FALSE(keys[i].is_keylocked()) << i;
-    }
-    for (int i = 0; i < kThreads; ++i) {
-      COERCE_ERROR(sessions[i].get_result());
-      sessions[i].release();
-    }
-
     COERCE_ERROR(engine.uninitialize());
   }
   cleanup_test(options);
