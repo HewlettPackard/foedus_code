b91672b00f8ef098d64a61eee598d86d2e571f1b
Hideaki Kimura <hideaki.kimura@hp.com>
2016-04-27 14:40:07 -0700

    Bug workaroun. Turns out there is a wrong assertion firing when there is non-durable meta log. So far worked it around by putting wait_for_commit() in this testcase. A real bug fix will follow. BTW also has aesthetic changes to meet our coding convention.

--- tests-core/src/foedus/restart/test_simple_bringup.cpp
+++ tests-core/src/foedus/restart/test_simple_bringup.cpp
@@ -23,19 +23,17 @@
 #include "foedus/engine_options.hpp"
 #include "foedus/test_common.hpp"
 #include "foedus/log/log_manager.hpp"
+#include "foedus/snapshot/snapshot_manager.hpp"
 #include "foedus/storage/storage_manager.hpp"
 #include "foedus/storage/array/array_metadata.hpp"
 #include "foedus/storage/array/array_storage.hpp"
 #include "foedus/storage/masstree/masstree_metadata.hpp"
 #include "foedus/storage/masstree/masstree_storage.hpp"
-#include "foedus/storage/sequential/sequential_metadata.hpp"
-#include "foedus/storage/sequential/sequential_storage.hpp"
 #include "foedus/xct/xct_manager.hpp"
 
-#include "foedus/snapshot/snapshot_manager.hpp"
 
 /**
- * @file test_restart_meta.cpp
+ * @file test_simple_bringup.cpp
  * Testcases for metadata log handling in restart.
  * No data operation, just metadata logs.
  */
@@ -43,13 +41,13 @@ namespace foedus {
 namespace restart {
 DEFINE_TEST_CASE_PACKAGE(SimpleBringupTest, foedus.restart);
 
-  //Create a masstree, quit while it is still empty, then try to
-  //open it again.
+  // Create a masstree, quit while it is still empty, then try to
+  // open it again.
   //
-  //This code results from attempting to follow the bare example given
-  //in the doxygen.  The goal was to just create a storage, see that
-  //there were files in the FS, then see that the storage was already
-  //created on restart.
+  // This code results from attempting to follow the bare example given
+  // in the doxygen.  The goal was to just create a storage, see that
+  // there were files in the FS, then see that the storage was already
+  // created on restart.
 TEST(SimpleBringupTest, Empty) {
   EngineOptions options = get_tiny_options();
   {
@@ -58,42 +56,41 @@ TEST(SimpleBringupTest, Empty) {
     {
       UninitializeGuard guard(&engine);
       foedus::storage::masstree::MasstreeMetadata mst_meta("my_masstree");
-      if(!engine.get_storage_manager()->get_storage("my_masstree")->exists()){
-	foedus::Epoch ep;    
-	foedus::storage::masstree::MasstreeMetadata mst_meta("my_masstree");
-	printf("++++++++~~~~~~~~~~make the storage\n\n\n");
-	engine.get_storage_manager()->create_storage(&mst_meta, &ep);
-	//Taking a snapshot doesn't help (or hurt).
-	//engine.get_snapshot_manager()->trigger_snapshot_immediate(true);
+      if (!engine.get_storage_manager()->get_storage("my_masstree")->exists()) {
+        foedus::Epoch ep;
+        foedus::storage::masstree::MasstreeMetadata mst_meta("my_masstree");
+        std::cout << "++++++++~~~~~~~~~~make the storage\n\n\n";
+        COERCE_ERROR(engine.get_storage_manager()->create_storage(&mst_meta, &ep));
+        // Taking a snapshot doesn't help (or hurt).
+        // engine.get_snapshot_manager()->trigger_snapshot_immediate(true);
+        COERCE_ERROR_CODE(engine.get_xct_manager()->wait_for_commit(ep));
       }
-      
+
       COERCE_ERROR(engine.uninitialize());
     }
   }
 
   {
-    //Absolutely certain this is a different engine
+    // Absolutely certain this is a different engine
     Engine engine(options);
 
-    printf("++++++++~~~~~~~~ we do get here\n\n\n");
-    
+    std::cout << "++++++++~~~~~~~~ we do get here\n\n\n";
     COERCE_ERROR(engine.initialize());
     {
-      printf("++++++++~~~~~~~~ we never get here\n\n\n");
+      std::cout << "++++++++~~~~~~~~ we never get here\n\n\n";
 
-      UninitializeGuard guard(&engine);      
+      UninitializeGuard guard(&engine);
       foedus::storage::masstree::MasstreeMetadata mst_meta("my_masstree");
-      //We never get here.
-      if(!engine.get_storage_manager()->get_storage("my_masstree")->exists()){
-	//Should never get here.
-	EXPECT_TRUE(false);
-	foedus::Epoch ep;    
-	foedus::storage::masstree::MasstreeMetadata mst_meta("my_masstree");
-	printf("++++++++~~~~~~~~~~makeymakey storage\n\n\n");
-	engine.get_storage_manager()->create_storage(&mst_meta, &ep);
-	engine.get_snapshot_manager()->trigger_snapshot_immediate(true);
+      // We never get here.
+      if (!engine.get_storage_manager()->get_storage("my_masstree")->exists()) {
+        // Should never get here.
+        foedus::Epoch ep;
+        foedus::storage::masstree::MasstreeMetadata mst_meta("my_masstree");
+        std::cout << "++++++++~~~~~~~~~~makeymakey storage\n\n\n";
+        COERCE_ERROR(engine.get_storage_manager()->create_storage(&mst_meta, &ep));
+        engine.get_snapshot_manager()->trigger_snapshot_immediate(true);
       }
-      
+
       COERCE_ERROR(engine.uninitialize());
     }
   }
